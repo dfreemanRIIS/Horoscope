ccff3d0a23d289b47c853d0406e0122160a2b2e6
dfreemanRIIS <dfreeman@riis.com>
2017-01-16 12:56:21 -0500

    Horoscope now uses array adapter rather than hard coded list

--- app/src/main/java/com/example/dfreeman/horoscope/MainActivity.java
+++ app/src/main/java/com/example/dfreeman/horoscope/MainActivity.java
@@ -1,18 +1,23 @@
 package com.example.dfreeman.horoscope;
 
-import android.support.v7.app.AppCompatActivity;
+import android.app.ListActivity;
 import android.os.Bundle;
 import android.content.Intent;
 import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
 import android.widget.ListView;
 import android.view.View;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends ListActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        ListView listView = getListView();
+
+        //Get the horoscope names and set them in the activity_main
+        ArrayAdapter<Horoscope> listAdapter = new ArrayAdapter<>(this, android.R.layout.simple_list_item_1, Horoscope.horoscopes);
+        listView.setAdapter(listAdapter);
 
         //Make an onClickListener for the main menu items
         AdapterView.OnItemClickListener itemClickListener = new AdapterView.OnItemClickListener() {
@@ -24,7 +29,6 @@ public class MainActivity extends AppCompatActivity {
         };
 
         //Add listener to list view
-        ListView listView = (ListView) findViewById(R.id.main_list);
         listView.setOnItemClickListener(itemClickListener);
     }
 }
