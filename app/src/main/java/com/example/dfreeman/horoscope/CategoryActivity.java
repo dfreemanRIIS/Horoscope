d4556a217d673bc899468a44343f5a81c913a4d8
dfreemanRIIS <dfreeman@riis.com>
2017-01-23 14:19:20 -0500

    Now pulls horoscope from API

--- app/src/main/java/com/example/dfreeman/horoscope/CategoryActivity.java
+++ app/src/main/java/com/example/dfreeman/horoscope/CategoryActivity.java
@@ -1,40 +1,178 @@
 package com.example.dfreeman.horoscope;
 
+import android.content.Context;
+import android.net.ConnectivityManager;
+import android.net.NetworkInfo;
+import android.os.AsyncTask;
 import android.os.Bundle;
-import android.app.ListActivity;
-import android.speech.tts.TextToSpeech;
 import android.support.v7.app.AppCompatActivity;
-import android.widget.ArrayAdapter;
-import android.widget.ListView;
+import android.util.Log;
 import android.widget.TextView;
-
-import org.w3c.dom.Text;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.URL;
 
 public class CategoryActivity extends AppCompatActivity {
 
     public static final String EXTRA_HOROSCOPENO = "horoscopeNo";
+    protected String nameThisIsMostLikelyBadPractice = "";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.display);
 
+        //Get current horoscope object
         int horoNo = (Integer) getIntent().getExtras().get(EXTRA_HOROSCOPENO);
         Horoscope horoscope = Horoscope.horoscopes[horoNo];
 
+        //Set name
         TextView name = (TextView) findViewById(R.id.name);
         name.setText(horoscope.getName());
+        //Set this for the URL later
+        //nameThisIsMostLikelyBadPractice = "http://a.knrz.co/horoscope-api/current/" + horoscope.getName();
 
+        //OLD set description
         TextView description = (TextView) findViewById(R.id.desc);
         description.setText(horoscope.getDescription());
 
+        //Set sign
         TextView sign = (TextView) findViewById(R.id.sign);
         sign.setText(horoscope.getSign());
 
+        //Set month
         TextView month = (TextView) findViewById(R.id.month);
         month.setText(horoscope.getMonth());
 
+        //OLD set horoscope
+        //TextView theeHoroscope = (TextView) findViewById(R.id.horoscope);
+        //theeHoroscope.setText(horoscope.getHoroscope());
+
+        //Do the magic stuff
+        checkNetworkConnection();
+        new HTTPAsyncTask().execute();
+    }
+
+    public boolean checkNetworkConnection() {
+        ConnectivityManager connMgr = (ConnectivityManager)getSystemService(Context.CONNECTIVITY_SERVICE);
+        NetworkInfo networkInfo = connMgr.getActiveNetworkInfo();
+        TextView netCon = (TextView)findViewById(R.id.networkConnection);
+        boolean isConnected = false;
+        if(networkInfo != null && (isConnected = networkInfo.isConnected())) {
+            netCon.setText("Network Connection Established");
+            netCon.setBackgroundColor(0xFF7CCC26);
+        } else {
+            netCon.setText("No Network Connection Established");
+            netCon.setBackgroundColor(0xFFFF0000);
+        }
+
+        return isConnected;
+    }
+
+    private String HttpGet(String myUrl) throws IOException {
+        InputStream inputStream = null;
+        String result = "";
+
+        URL url = new URL(myUrl);
+        HttpURLConnection conn = (HttpURLConnection)url.openConnection();
+        conn.connect();
+        inputStream = conn.getInputStream();
+        if(inputStream != null) {
+            result = convertInputStreamToString(inputStream);
+        } else {
+            result = "Did not work!";
+        }
+        return result;
+    }
+
+    private class HTTPAsyncTask extends AsyncTask<Void, Void, String> {
+        @Override
+        protected String doInBackground(Void... params) {
+            // These two need to be declared outside the try/catch
+            // so that they can be closed in the finally block.
+            HttpURLConnection urlConnection = null;
+            BufferedReader reader = null;
+
+            // Will contain the raw JSON response as a string.
+            String forecastJsonStr = null;
+
+            try {
+
+                URL url = new URL("http://a.knrz.co/horoscope-api/current/gemini");
+
+                // Create the request to OpenWeatherMap, and open the connection
+                urlConnection = (HttpURLConnection) url.openConnection();
+                urlConnection.setRequestMethod("GET");
+                urlConnection.connect();
+
+                // Read the input stream into a String
+                InputStream inputStream = urlConnection.getInputStream();
+                StringBuffer buffer = new StringBuffer();
+                if (inputStream == null) {
+                    // Nothing to do.
+                    return null;
+                }
+                reader = new BufferedReader(new InputStreamReader(inputStream));
+
+                String line;
+                while ((line = reader.readLine()) != null) {
+                    // Since it's JSON, adding a newline isn't necessary (it won't affect parsing)
+                    // But it does make debugging a *lot* easier if you print out the completed
+                    // buffer for debugging.
+                    buffer.append(line + "\n");
+                }
+
+                if (buffer.length() == 0) {
+                    // Stream was empty.  No point in parsing.
+                    return null;
+                }
+                forecastJsonStr = buffer.toString();
+                return forecastJsonStr;
+            } catch (IOException e) {
+                Log.e("PlaceholderFragment", "Error ", e);
+                // If the code didn't successfully get the weather data, there's no point in attemping
+                // to parse it.
+                return null;
+            } finally{
+                if (urlConnection != null) {
+                    urlConnection.disconnect();
+                }
+                if (reader != null) {
+                    try {
+                        reader.close();
+                    } catch (final IOException e) {
+                        Log.e("PlaceholderFragment", "Error closing stream", e);
+                    }
+                }
+            }
+        }
+
+        @Override
+        protected void onPostExecute(String s) {
+            super.onPostExecute(s);
+            TextView theeHoroscope = (TextView) findViewById(R.id.horoscope);
+            theeHoroscope.setText(s);
+            //tvWeatherJson.setText(s);
+            Log.i("json", s);
+        }
+    }
+
+    private static String convertInputStreamToString(InputStream inputStream) throws IOException {
+        BufferedReader bufferedReader = new BufferedReader( new InputStreamReader(inputStream));
+        String line = "";
+        String result = "";
+        while((line = bufferedReader.readLine()) != null)
+            result += line;
+
+        inputStream.close();
+        return result;
+    }
+
+    protected void setError(int NOPE) {
         TextView theeHoroscope = (TextView) findViewById(R.id.horoscope);
-        theeHoroscope.setText(horoscope.getHoroscope());
+        theeHoroscope.setText("Something failed NOPE" + NOPE);
     }
 }
